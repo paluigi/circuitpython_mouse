0b99df1426df56d456a975e87b94eae2030a0c7c
Luigi <paluigi@gmail.com>
2022-12-26 17:59:27 +0100

    completed keyboard integration - only 1 connection

--- code.py
+++ code.py
@@ -3,8 +3,10 @@ import board
 import digitalio
 import usb_hid
 from adafruit_hid.mouse import Mouse
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
 from adafruit_debouncer import Debouncer
-
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
@@ -13,6 +15,9 @@ from adafruit_ble.services.nordic import UARTService
 time.sleep(10)
 # Setup mouse
 m = Mouse(usb_hid.devices)
+# Setup keyboard
+keyboard = Keyboard(usb_hid.devices)
+keyboard_layout = KeyboardLayoutUS(keyboard)
 # Setup LED
 led = digitalio.DigitalInOut(board.INVERTED_LED)
 led.direction = digitalio.Direction.OUTPUT
@@ -34,36 +39,32 @@ ble.start_advertising(advertisement)
 print("Waiting to connect")
 while not ble.connected:
     pass
-    
 
-speed = 0.5
+speed = 0.1
 
 while True:
-
-    while ble.connected:
-        switch.update()
-        if switch.fell:
-            status = not status
-        if status == True:
-            led.value = False
-            m.move(80,0,0)
-            time.sleep(speed)
-            m.move(0,80,0)
-            time.sleep(speed)
-            m.move(-80,0,0)
-            time.sleep(speed)
-            m.move(0,-80,0)
-            time.sleep(speed)
-        else:
-            led.value = True
-            
+    switch.update()
+    if switch.fell:
+        status = not status
+    if status == True:
+        led.value = False
+        m.move(80,0,0)
+        time.sleep(speed)
+        m.move(0,80,0)
+        time.sleep(speed)
+        m.move(-80,0,0)
+        time.sleep(speed)
+        m.move(0,-80,0)
+        time.sleep(speed)
+    else:
+        led.value = True
+    if ble.connected:
         s = uart.readline()
         if s:
             try:
-                result = str(eval(s))
+                keyboard_layout.write(str(s.decode()))
             except Exception as e:
-                result = repr(e)
-            uart.write("HELLO!".encode("utf-8"))
+                print(e)
+                uart.write(f"{e} - ERROR\n".encode("utf-8"))
+            uart.write(f"{str(s.decode())} - DONE\n".encode("utf-8"))
 
-
-    
