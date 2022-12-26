c84012468ef905744569e157a4a3752cee256e76
danielemarsico <marsicod@gmail.com>
2022-12-26 16:53:41 +0100

    added bluetooth support
    added bluetooth support

--- code.py
+++ code.py
@@ -5,6 +5,9 @@ import usb_hid
 from adafruit_hid.mouse import Mouse
 from adafruit_debouncer import Debouncer
 
+from adafruit_ble import BLERadio
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+from adafruit_ble.services.nordic import UARTService
 
 # Wait at the beginning
 time.sleep(10)
@@ -21,19 +24,46 @@ switch = Debouncer(button)
 # Setup Status
 status = True
 
+
+ble = BLERadio()
+uart = UARTService()
+#uart.init(timeout=1000) # init with given parameters
+advertisement = ProvideServicesAdvertisement(uart)
+
+ble.start_advertising(advertisement)
+print("Waiting to connect")
+while not ble.connected:
+    pass
+    
+
+speed = 0.5
+
 while True:
-    switch.update()
-    if switch.fell:
-        status = not status
-    if status == True:
-        led.value = False
-        m.move(80,0,0)
-        time.sleep(0.1)
-        m.move(0,80,0)
-        time.sleep(0.1)
-        m.move(-80,0,0)
-        time.sleep(0.1)
-        m.move(0,-80,0)
-        time.sleep(0.1)
-    else:
-        led.value = True
+
+    while ble.connected:
+        switch.update()
+        if switch.fell:
+            status = not status
+        if status == True:
+            led.value = False
+            m.move(80,0,0)
+            time.sleep(speed)
+            m.move(0,80,0)
+            time.sleep(speed)
+            m.move(-80,0,0)
+            time.sleep(speed)
+            m.move(0,-80,0)
+            time.sleep(speed)
+        else:
+            led.value = True
+            
+        s = uart.readline()
+        if s:
+            try:
+                result = str(eval(s))
+            except Exception as e:
+                result = repr(e)
+            uart.write("HELLO!".encode("utf-8"))
+
+
+    
